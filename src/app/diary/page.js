be2a0fff54a1e30181bb5c6b53dfe8fdb6b6ed80
marcelindasr <marcelindasr@gmail.com>
2023-11-21 09:26:29 +0800

    chore : diary render logic, introduce 'last-item'

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -7,20 +7,28 @@ import axios from "axios";
 export default function Diary() {
   const [judul, setJudul] = useState([]);
   const [isiDiary, setIsiDiary] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); 
 
   const endpointAPI = "https://6555c4c184b36e3a431e4a9e.mockapi.io/diaryku";
 
   async function getDiary() {
-    const res = await axios.get(endpointAPI);
-    const data = res.data;
+    try {
+      const res = await axios.get(endpointAPI);
+      const data = res.data;
 
-    //ambil judul
-    const judul = data.map((item) => item.judul);
-    setJudul(judul);
+      //ambil judul
+      const judul = data.map((item) => item.judul);
+      setJudul(judul);
 
-    //ambil isi_diary
-    const isi_diary = data.map((item) => item.isi_diary);
-    setIsiDiary(isi_diary);
+      //ambil isi_diary
+      const isi_diary = data.map((item) => item.isi_diary);
+      setIsiDiary(isi_diary);
+
+      setIsLoading(false); // Set loading to false after data is fetched
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setIsLoading(false); // Set loading to false in case of an error
+    }
   }
 
   useEffect(() => {
@@ -29,16 +37,17 @@ export default function Diary() {
 
   return (
     <div>
-      {judul.length > 0 ? (
+      {isLoading ? ( // Check loading state
+        "API is loading"
+      ) : judul.length > 0 ? (
         <ul>
           {judul.map((item, idx) => (
-              <li>
-                <div className="diary-container">
-                    <h1>{judul[idx]}</h1>
-                    <p className="p-diary">{isiDiary[idx]}</p>
-                </div>
-              </li>
-
+            <li key={idx}> {/* Add key prop for list items */}
+              <div className={`diary-container ${idx === judul.length -1? 'last-item' : ''}`}>
+                <h1>{judul[idx]}</h1>
+                <p className="p-diary">{isiDiary[idx]}</p>
+              </div>
+            </li>
           ))}
         </ul>
       ) : (
